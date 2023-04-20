0b8f15c8ba1fd37e642a951fdef22ddf6280fa28
mepp03 <98363173+mepp03@users.noreply.github.com>
2023-04-20 17:59:30 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,91 +1,51 @@
-function mostrarVotos()
+function checarUsuario()
 {
-    var nome = "leandro";
-    fetch("https://mepp03.github.io/Votacao-Melhores-Animes/votos2021Winter.json")
-        .then(response => response.json())
-        .then(data =>
-        {
-            data = data.votos2021Winter;
-            console.log(data[0].abertura[nome].primeiro.nomeJ);
-
-            data[0].abertura[nome].primeiro = Object.assign(data[0].abertura[nome].primeiro,
-                {
-                    id: "114194",
-                    nomeJ: "nomeJVoto",
-                    nomeE: "nomeEVoto",
-                    imagem: "imagemVoto",
-                    extra: "extraVoto",
-                    ponto: "pontuacao"
-                });
-                console.log(data[0].abertura[nome].primeiro.nomeJ);
-
-                fetch("https://mepp03.github.io/Votacao-Melhores-Animes/votos2021Winter.json",
-                {
-                    method: 'POST',
-                    body: JSON.stringify(data),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-                .then(response => response.json())
-                .then(data =>
-                {
-                    console.log('Dados salvos com sucesso:', data);
-                })
-                .catch(error =>
-                {
-                    console.error(error);
-                });
-        });
+    // localStorage.setItem("usuario", "leandro");
+    // localStorage.setItem("senha", "leandro");
+    var usuario = localStorage.getItem("usuario");
+    
+    if (usuario != "null")
+    {
+        document.getElementById("botaoSair").classList.remove("esconder");
+        document.getElementById("botaoVotar").classList.remove("esconder");
+        document.getElementById("botaoEntrar").classList.add("esconder");
+    } 
+    else
+    {
+        document.getElementById("cabecalho__temporadas").classList.add("desabilitar");
+    }
 }
 
-// fetch(`http://localhost:3000/votos2021Winter/${idCat}`)
-//     .then(response => response.json())
-//     .then(data =>
-//     {
-//         dados = data;
+function abrirLogin()
+{
+    modal.style.display = "block";
+}
 
-//         switch (posicao) 
-//         {
-//             case "1":
-//                 ordinal = "primeiro";
-//                 pontuacao = "3";
-//                 break;
-//             case "2":
-//                 ordinal = "segundo";
-//                 pontuacao = "2";
-//                 break;
-//             case "3":
-//                 ordinal = "terceiro";
-//                 pontuacao = "1";
-//                 break;
-//         }
+function logar()
+{    
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+    var usuarios = [{nome: 'leandro', senha: 'leandro'}, {nome: 'thiago', senha: 'thiago'}, {nome: 'nil', senha: 'nil'}];
 
-//         dados[tipo].leandro[ordinal] = Object.assign(dados[tipo].leandro[ordinal],
-//             {
-//                 id: idVoto,
-//                 nomeJ: nomeJVoto,
-//                 nomeE: nomeEVoto,
-//                 imagem: imagemVoto,
-//                 extra: extraVoto,
-//                 ponto: pontuacao
-//             });
+    for (var i = 0; i < usuarios.length; i++) {
+        if (usuario == usuarios[i].nome && senha == usuarios[i].senha) {
+            localStorage.setItem('usuario', usuario);
+            location.reload();
+            return;
+        }
+    }
 
-//         fetch(`http://localhost:3000/votos2021Winter/${idCat}`,
-//             {
-//                 method: 'PUT',
-//                 body: JSON.stringify(dados),
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 }
-//             })
-//             .then(response => response.json())
-//             .then(dados =>
-//             {
-//                 console.log('Dados salvos com sucesso:', dados);
-//             })
-//             .catch(error =>
-//             {
-//                 console.error('Erro ao salvar dados:', error);
-//             });
-//     });
+    alert("Usuário ou senha incorretos");
+}
+
+function deslogar()
+{
+    localStorage.setItem("usuario", null);
+    location.reload();
+}
+
+function votar()
+{
+    
+    window.location.href = "votacaoCopy.html";
+}

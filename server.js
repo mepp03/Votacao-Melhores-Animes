424d43bfc46a7d035f30b7243fd16fbd2cebae86
Mepp <black.mepp@gmail.com>
2025-10-31 16:54:26 -0300

    arrumando cors

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("dados/dbVotos.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({
+  static: "./public",
+});
 
 const PORT = process.env.PORT || 3000;
+
+// Configurar CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
